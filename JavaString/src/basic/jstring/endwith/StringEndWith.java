3248cee2ca3a780f09b75aabb207f3bb3d33d0a4
huy nguyen <huyhumg2019@gmail.com>
2019-07-25 14:06:02 +0900

    String endWith

---
+++ JavaString/src/basic/jstring/endwith/StringEndWith.java
@@ -0,0 +1,12 @@
+package basic.jstring.endwith;
+
+public class StringEndWith {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String endWith="java web";
+		System.out.println(endWith.endsWith("b"));
+
+	}
+
+}
