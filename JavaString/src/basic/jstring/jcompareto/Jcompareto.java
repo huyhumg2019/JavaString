aa1f16f936e81c258177673cf62e5105ec243e8d
huy nguyen <huyhumg2019@gmail.com>
2019-07-25 12:36:37 +0900

    String

---
+++ JavaString/src/basic/jstring/jcompareto/Jcompareto.java
@@ -0,0 +1,20 @@
+package basic.jstring.jcompareto;
+
+public class Jcompareto {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1 = "a";
+		String s2 = "b";
+		String s3 = "c";
+		String s4 = "d";
+		// so sánh chuỗi theo thứ tự bảng chữ cái  
+		System.out.println(s1.compareTo(s2));
+		System.out.println(s1.compareTo(s3));
+		System.out.println(s1.compareTo(s4));
+		System.out.println(s4.compareTo(s2));
+		
+
+	}
+
+}
