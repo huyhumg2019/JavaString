09d230e315840f9eafdd351a978d17f891310670
huy nguyen <huyhumg2019@gmail.com>
2019-07-25 12:54:57 +0900

    string Concat

---
+++ JavaString/src/basic/jstring/concat/StringConcat.java
@@ -0,0 +1,13 @@
+package basic.jstring.concat;
+
+public class StringConcat{
+
+public static void  main (String []args) {
+	// noi 2 chuoi 
+	String s= "java";
+	s=s.concat(" la ngon ngu ntn");
+	System.out.println(s);
+	
+	
+}
+}
