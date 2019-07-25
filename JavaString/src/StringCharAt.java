7d44c23dafd90a28a47890c5afdb12b6b33c580c
huy nguyen <huyhumg2019@gmail.com>
2019-07-25 12:16:26 +0900

    String charAt

---
+++ JavaString/src/StringCharAt.java
@@ -0,0 +1,15 @@
+
+public class StringCharAt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// index    0123456789  
+		String s = "hello word";
+		char cAt= s.charAt(4);
+		char cat2v= s.charAt(5);
+		System.out.println(cAt);
+		System.out.println(cat2v);
+		System.out.println(s.length());
+	}
+
+}
