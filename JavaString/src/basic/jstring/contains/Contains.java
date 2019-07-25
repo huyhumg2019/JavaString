f4d882541d43c44005e11b1c73147d444c9de07c
huy nguyen <huyhumg2019@gmail.com>
2019-07-25 12:55:09 +0900

    String Contains

---
+++ JavaString/src/basic/jstring/contains/Contains.java
@@ -0,0 +1,15 @@
+package basic.jstring.contains;
+
+public class Contains {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// tra ve true neu chuoi gia tri char dung . phan biet dau cach
+		String s = "toi muon tro thanh lap trinh vien java web";
+		System.out.println(s.contains("muontrothanh"));
+		System.out.println(s.contains("java web"));
+		System.out.println(s.contains("lap trinh v"));
+
+	}
+
+}
