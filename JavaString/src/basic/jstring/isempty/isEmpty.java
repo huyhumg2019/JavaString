8fb472676456a20cacf0cc3c2c96232ec7e37e4f
huy nguyen <huyhumg2019@gmail.com>
2019-07-25 15:09:57 +0900

    String isEmpty

---
+++ JavaString/src/basic/jstring/isempty/isEmpty.java
@@ -0,0 +1,15 @@
+package basic.jstring.isempty;
+
+public class isEmpty {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// kiem tra chuoi rong hay ko
+		String a ="";
+		String b ="sdasa";
+		System.out.println(a.isEmpty());
+		System.out.println(b.isEmpty());
+
+	}
+
+}
