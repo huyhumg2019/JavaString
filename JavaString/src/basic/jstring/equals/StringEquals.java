d569d5e8d092b93089b81aa375672bec4707133d
huy nguyen <huyhumg2019@gmail.com>
2019-07-25 14:09:51 +0900

    String equals

---
+++ JavaString/src/basic/jstring/equals/StringEquals.java
@@ -0,0 +1,15 @@
+package basic.jstring.equals;
+
+public class StringEquals {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// kiem tra 2 chuoi dung hay sai
+		String a = "Java";
+		String b = "JAVA";
+		String c = "Java";
+		System.out.println(a.equals(b));
+		System.out.println(a.equals(c));
+	}
+
+}
