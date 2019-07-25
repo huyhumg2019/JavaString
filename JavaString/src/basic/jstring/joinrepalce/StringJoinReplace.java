11f8264e25597b751e323d0a3012fa0a22d52c92
huy nguyen <huyhumg2019@gmail.com>
2019-07-25 15:31:46 +0900

    Join Replace Valueof

---
+++ JavaString/src/basic/jstring/joinrepalce/StringJoinReplace.java
@@ -0,0 +1,13 @@
+package basic.jstring.joinrepalce;
+
+public class StringJoinReplace {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			String a =String.join(" ", "java","web","huy","pro");
+			System.out.println(a);
+    // replace thay doi chuoi 
+			System.out.println(a.replace("a", "u"));
+	}
+
+}
---
+++ JavaString/src/basic/jstring/valueof/StringValueOf.java
@@ -0,0 +1,14 @@
+package basic.jstring.valueof;
+
+public class StringValueOf {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// chuyen kieu sang kieu string
+		int s = 30 ;
+		String b = String.valueOf(s);
+		System.out.println(b+10);
+
+	}
+
+}
